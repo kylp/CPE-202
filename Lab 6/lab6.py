a3aec2811c511eefca6f263c3818536bb9931f78
kylp <97jark@gmail.com>
2020-02-23 00:23:19 -0800

    updated lab 6

--- Lab 6/lab6.py
+++ Lab 6/lab6.py
@@ -2,10 +2,6 @@
 import random
 import time
 
-random.seed(1)
-
-alist = random.sample(range(10000), 10)
-
 
 def insertion_sort(arr):
     size = len(arr)
@@ -14,10 +10,28 @@ def insertion_sort(arr):
         while j > 0 and arr[j-1] > arr[j]:
             arr[j - 1], arr[j], = arr[j], arr[j-1]
             j -= 1
-        return arr
+    return arr
 
 
-start_time = time.time()
-    #insert algorithm here
-end_time = time.time()
-sort_time = end_time - start_time
+def algotime(arrsize):
+    print("Timing insertion sort on " + str(arrsize) + " items...")
+    random.seed(1)
+    alist = list(range(arrsize))
+    random.shuffle(alist)
+    start_time = time.time()
+    insertion_sort(alist)
+    end_time = time.time()
+    sort_time = end_time - start_time
+    print("Done! Total time: " + str(sort_time))
+    return sort_time
+
+
+print(algotime(1000))
+print(algotime(2000))
+print(algotime(4000))
+print(algotime(8000))
+print(algotime(16000))
+print(algotime(32000))
+print(algotime(100000))
+print(algotime(500000))
+
