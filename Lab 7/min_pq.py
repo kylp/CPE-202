a9dc4cf235d6c5eb5254844163adac3fa4a335f8
kylp <97jark@gmail.com>
2020-02-23 17:14:27 -0800

    updated lab 7 file

--- Lab 7/min_pq.py
+++ Lab 7/min_pq.py
@@ -10,26 +10,50 @@ Author:
 
 def index_parent(index):
     if index <= 0:
-        raise IndexError()
+        return -1
     return (index - 1) // 2
 
 
 def index_left(index):
     if index < 0:
-        raise IndexError()
+        return -1
     return 2 * index + 1
 
 
 def index_right(index):
     if index < 0:
-        raise IndexError()
+        return -1
     return 2 * index + 2
 
 
-def index_minchild(arr, index, capacity):
-    if index_right(index) >= capacity:
+def index_minchild(arr, index, num_items):
+    if index_left(index) >= num_items:
+        return -1
+    if index_right(index) >= num_items:
         return index_left(index)
-    
+    if arr[index_left(index)] < arr[index_right(index)]:
+        return index_left(index)
+    return index_right(index)
+
+
+def shift_down(arr, index, num_items):
+    imin = index_minchild(arr, index, num_items)
+    if imin < 0 or arr[imin] >= arr[index]:
+        return None
+    temp = arr[index]
+    arr[index] = arr[imin]
+    arr[imin] = temp
+    return shift_down(arr, imin, num_items)
+
+
+def shift_up(arr, index):
+    iparent = index_parent(index)
+    if iparent < 0 or arr[iparent] <= arr[index]:
+        return
+    temp = arr[index]
+    arr[index] = arr[iparent]
+    arr[iparent] = temp
+    return shift_up(arr, iparent)
 
 
 class MinPQ:
@@ -39,6 +63,7 @@ class MinPQ:
         num_items (int): the number of items in the queue.
         arr (list): an array which contains the items in the queue.
     """
+
     def __init__(self, arr=None):
         if arr is None:
             self.capacity = 2
@@ -47,13 +72,13 @@ class MinPQ:
         else:
             self.capacity = len(arr)
             self.num_items = len(arr)
-            self.arr = arr
+            self.arr = self.heapify(arr)
 
     def __eq__(self, other):
         return isinstance(other, MinPQ) \
-            and self.capacity == other.capacity \
-            and self.num_items == other.num_items \
-            and self.arr == other.arr
+               and self.capacity == other.capacity \
+               and self.num_items == other.num_items \
+               and self.arr == other.arr
 
     def __repr__(self):
         return 'MinPQ(%d, %d, %s)' % (self.capacity, self.num_items, repr(self.arr))
@@ -65,8 +90,11 @@ class MinPQ:
         Returns:
             None : it returns nothing
         """
-
-
+        i = index_parent(len(arr) - 1)
+        while i >= 0:
+            shift_down(arr, i, len(arr))
+            i -= 1
+        return arr
 
     def insert(self, item):
         """inserts an item to the queue
@@ -77,7 +105,12 @@ class MinPQ:
         Returns:
             None : it returns nothing
         """
-        pass
+        if self.num_items == self.capacity:
+            self.enlarge()
+        self.arr[self.num_items] = item
+        shift_up(self.arr, self.num_items)
+        self.num_items += 1
+        return
 
     def del_min(self):
         """deletes the minimum item in the queue
@@ -88,7 +121,13 @@ class MinPQ:
         Raises:
             IndexError : Raises IndexError when the queue is empty
         """
-        pass
+        min_item = self.arr[0]
+        self.arr[0] = self.arr[self.num_items-1]
+        shift_down(self.arr, 0, self.num_items - 1)
+        self.num_items -= 1
+        if self.capacity > 2 and 0 < self.num_items <= self.capacity // 4:
+            self.shrink()
+        return min_item
 
     def min(self):
         """returns the minimum item in the queue without deleting the item
@@ -97,28 +136,42 @@ class MinPQ:
         Raises:
             IndexError : Raises IndexError when the queue is empty
         """
-        pass
+        if self.arr is None:
+            raise IndexError()
+        return self.arr[0]
 
     def is_empty(self):
         """checks if the queue is empty 
         Returns:
             bool : True if empty, False otherwise. 
         """
-        pass 
-    
+        if self.num_items == 0:
+            return True
+        return False
+
     def size(self):
         """returns the number of items in the queue 
         Returns:
             int : it returns the number of items in the queue 
         """
-        pass 
+        return self.num_items
 
     def enlarge(self):
         """enlarges the array.
         """
-        pass
+        self.capacity = 2 * self.capacity
+        temp = [None] * self.capacity
+        for i in range(self.num_items):
+            temp[i] = self.arr[i]
+        self.arr = temp
 
     def shrink(self):
         """shrinks the array.
         """
-        pass
+        if self.num_items > 2 * self.capacity:
+            raise ValueError
+        self.capacity = self.capacity // 2
+        temp = [None] * self.capacity
+        for i in range(self.num_items):
+            temp[i] = self.arr[i]
+        self.arr = temp
